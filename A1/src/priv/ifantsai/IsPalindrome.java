167c97088a1ef4b131b26cf9796b26e47da38eb0
t100ta <t100ta@asus>
2018-04-20 11:38:11 +0800

    advance 1

---
+++ A1/src/priv/ifantsai/IsPalindrome.java
@@ -0,0 +1,34 @@
+package priv.ifantsai;
+
+/*
+ * 题目:
+ * 给定一个数,判断该数是否为回文数
+ */
+public class IsPalindrome {
+	public static void main(String[] args) {
+		int num = 12321;
+		System.out.println(isPalindrome(num));
+	}
+	
+	public static boolean isPalindrome(int num) {
+		int help = 1;
+		// 如果num为12321, 则将help设置为10000
+//		while (help * 10 <= num) {      // 可能会产生溢出,导致死循环
+//			help *= 10;
+//		} 
+		while (num / help >= 10) {      // 所以将乘法转为除法
+			help *= 10;
+		}
+		while (num != 0) {
+			// 比较num最高位的数和最低位的数是否相等
+			if (num / help != num % 10) {
+				return false;
+			}
+			// 把num最高位和最低位的数给剥掉
+			num = num % help / 10;
+			// help减少两位
+			help /= 100;
+		}
+		return true;
+	}
+}
